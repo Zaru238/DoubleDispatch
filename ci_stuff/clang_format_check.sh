6fe9b52dabcc3b5b506f12cccfad61ac38018dfe
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-11 08:56:18 +0300

    Playing

--- ci_stuff/clang_format_check.sh
+++ ci_stuff/clang_format_check.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-git diff --cached --name-only --diff-filter=ACMRT |
-grep -E -i ".*\.(h|hpp|c|cpp)" |
-xargs clang-format -style=file -output-replacements-xml |
-grep "<replacement " >/dev/null
-if [ $? -ne 1 ]; then
-	echo "Commit didn't pass clang-format check"
-	exit 1
-fi
+echo "Test 1 2 3"
+
+git diff --cached --name-only --diff-filter=ACMRT #|
+#grep -E -i ".*\.(h|hpp|c|cpp)" |
+#xargs clang-format -style=file -output-replacements-xml |
+#grep "<replacement " >/dev/null
+#if [ $? -ne 1 ]; then
+#	echo "Commit didn't pass clang-format check"
+#	exit 1
+#fi
 
 exit
