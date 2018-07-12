6880d05bfe7374abe27270b7390351e15e43e572
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-12 21:53:14 +0300

    Another fix

--- ci_stuff/clang_format_check.sh
+++ ci_stuff/clang_format_check.sh
@@ -2,13 +2,14 @@
 
 echo "Test 1 2 3"
 
-git diff --cached --name-only --diff-filter=ACMRT #|
+find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp"
+#git diff --cached --name-only --diff-filter=ACMRT #|
 #grep -E -i ".*\.(h|hpp|c|cpp)" |
-#xargs clang-format -style=file -output-replacements-xml |
-#grep "<replacement " >/dev/null
-#if [ $? -ne 1 ]; then
-#	echo "Commit didn't pass clang-format check"
-#	exit 1
-#fi
+xargs clang-format -style=file -output-replacements-xml |
+grep "<replacement " >/dev/null
+if [ $? -ne 1 ]; then
+	echo "Commit didn't pass clang-format check"
+	exit 1
+fi
 
 exit
