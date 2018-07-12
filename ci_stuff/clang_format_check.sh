ee3636900c9d6f7f4ccfc3f6b4bfd7637615f8a0
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-12 22:52:03 +0300

    Another fix

--- ci_stuff/clang_format_check.sh
+++ ci_stuff/clang_format_check.sh
@@ -2,7 +2,7 @@
 
 echo "Test 1 2 3"
 
-find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp"
+find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
 #git diff --cached --name-only --diff-filter=ACMRT #|
 #grep -E -i ".*\.(h|hpp|c|cpp)" |
 xargs clang-format -style=file -output-replacements-xml |
