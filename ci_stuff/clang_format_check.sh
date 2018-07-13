1c16f0fcfd32491f2e57f7cd645c45838f5663cb
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-13 07:34:33 +0300

    Check git hook

--- ci_stuff/clang_format_check.sh
+++ ci_stuff/clang_format_check.sh
@@ -4,7 +4,7 @@ find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
 xargs clang-format -style=file -output-replacements-xml |
 grep "<replacement " >/dev/null
 if [ $? -ne 1 ]; then
-	echo "Commit didn't pass clang-format check"
+	echo "Commit didn't pass clang-format check d"
 	exit 1
 fi
 
