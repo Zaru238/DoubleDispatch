8023ed4ca2d9093291dbbee646e5ba9f5a2ba2db
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-13 07:29:47 +0300

    Test git hook

--- ci_stuff/clang_format_check.sh
+++ ci_stuff/clang_format_check.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-echo "Test 1 2 3"
-
 find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
-#git diff --cached --name-only --diff-filter=ACMRT #|
-#grep -E -i ".*\.(h|hpp|c|cpp)" |
 xargs clang-format -style=file -output-replacements-xml |
 grep "<replacement " >/dev/null
 if [ $? -ne 1 ]; then
