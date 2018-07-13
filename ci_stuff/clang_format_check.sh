b553c8eaf47f059bb9bdfe4c566bddab1b1fafc0
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-13 09:05:10 +0300

    Another fix

--- ci_stuff/clang_format_check.sh
+++ ci_stuff/clang_format_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" #|
-#xargs clang-format -style=file -output-replacements-xml |
+find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
+xargs clang-format -style=file -output-replacements-xml #|
 #grep "<replacement " >/dev/null
 #if [ $? -ne 1 ]; then
 #	echo "Commit didn't pass clang-format check"
