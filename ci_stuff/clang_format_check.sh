71a2761c751fc1101387f9933734a57b2b57d9b7
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-14 22:47:08 +0300

    Minor fix

--- ci_stuff/clang_format_check.sh
+++ ci_stuff/clang_format_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
-xargs clang-format-6.0 -style=file -output-replacements-xml #|
+xargs clang-format-6.0 -style=file -output-replacements-xml |
 grep "<replacement " >/dev/null
 if [ $? -ne 1 ]; then
 	echo "Commit didn't pass clang-format check"
